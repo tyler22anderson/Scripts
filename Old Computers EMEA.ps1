5b31226f3c6ce43c196464b3c37cffd7bc2004e5
tyler22anderson <64666886+tyler22anderson@users.noreply.github.com>
2020-05-07 14:27:30 -0400

    Update Old Computers EMEA.ps1

--- Old Computers EMEA.ps1
+++ Old Computers EMEA.ps1
@@ -1,12 +1,12 @@
 ﻿# Gets time stamps for all computers in the domain that have NOT logged in since after specified date 
 # Mod by Tilo 2013-08-27 
 import-module activedirectory  
-$domain = "corp.tenablesecurity.com"  
+$domain = "example.domain.com"  
 $DaysInactive = 90  
 $time = (Get-Date).Adddays(-($DaysInactive)) 
   
 # Get all AD computers with lastLogonTimestamp less than our time 
-Get-ADComputer -Filter {LastLogonTimeStamp -lt $time} -Properties LastLogonTimeStamp -SearchBase "OU=EMEA,OU=TenableComputers,DC=corp,DC=tenablesecurity,DC=com" | 
+Get-ADComputer -Filter {LastLogonTimeStamp -lt $time} -Properties LastLogonTimeStamp -SearchBase "OU=EMEA" | 
   
 # Output hostname and lastLogonTimestamp into CSV 
 select-object Name,@{Name="Stamp"; Expression={[DateTime]::FromFileTime($_.lastLogonTimestamp)}} | export-csv c:\users\tanderson\desktop\OLD_Computer-EMEA.csv -notypeinformation
