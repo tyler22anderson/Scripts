f6146a075addd9da61380514012de747e3fc7091
tyler22anderson <64666886+tyler22anderson@users.noreply.github.com>
2020-05-07 14:30:34 -0400

    Delete rosie_assets.py

--- rosie_assets.py
+++
@@ -1,8 +0,0 @@
-# basics that we import for every plugin
-import re
-from slackbot.bot import respond_to
-from slackbot.bot import listen_to
-import logging
-import rutils
-logger = logging.getLogger(__name__)
-import rdb
