866e6b34619cf356ba996b79a6cfa9877219aabf
tyler22anderson <64666886+tyler22anderson@users.noreply.github.com>
2020-05-07 14:25:58 -0400

    Update Okta-API-app-members.ps1

--- Okta-API-app-members.ps1
+++ Okta-API-app-members.ps1
@@ -7,7 +7,7 @@ $headers.Add("accept","application/json")
 #$headers.Add("Content-Type: application/json")
 $headers.Add("Authorization", "SSWS $api")
 
-$URL = "https://tenable.okta.com/api/v1/apps/0oa12vsqgaf4YRVvb0i8/users"
+$URL = "https://example.okta.com/api/v1/apps/0oa12vsqgaf4YRVvb0i8/users"
 
 $output = Invoke-RestMethod -Uri $URL -Headers $headers -ContentType "application/json" -Verbose | Select-Object "Credentials" 
 
